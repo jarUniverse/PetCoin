f57b60bbc64bfb7f07ff693a0c341af1d0d94f5e
jarUniverse <rizkifajar1199@gmail.com>
2022-09-26 11:21:10 +0000

    new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,7 @@
 module.exports = {
   content: ['./public/**/*.{html, js}'],
   theme: {
-    extend: {
-      zIndex: {
-        '1' : '1',
-        '2' : '2',
-        '5' : '5',
-        '8' : '8',
-        '9' : '9',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
